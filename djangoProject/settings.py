c1ef0def051d1350db8f56b419aec30da998e512
AstaWisdom <90654502+AstaWisdom@users.noreply.github.com>
2022-06-13 20:56:51 +0430

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u2r3u!@pbd51tty!+sjf8#1$ivx@6l=tzz9+=1rq4)sy8=09!$'
+SECRET_KEY = 'YOUR SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'djangoProject/images/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/images/')
 
 # My settings
 LOGIN_URL = 'users:login'
@@ -154,8 +154,8 @@ TIME_ZONE = 'Iran'
 STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = 'notesinkeep@gmail.com'
+EMAIL_HOST_USER = 'YOUR GMAIL'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = 'Pythonisreal12345!'
+EMAIL_HOST_PASSWORD = 'YOUR GMAIL PASSWORD'
